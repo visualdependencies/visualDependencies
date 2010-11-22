583975a221ea41a95654864bda4cd8a776c3bb48
knallisworld <knallisworld@56744f29-0aaf-9aa6-4f69-6ecd08e98722>
2010-11-22 23:05:10 +0000

    Share project "vdb-mysql-plugin" into "https://visualdependencies.googlecode.com/svn"
    git-svn-id: https://visualdependencies.googlecode.com/svn/vdb@18 56744f29-0aaf-9aa6-4f69-6ecd08e98722

---
+++ vdb-mysql-plugin/trunk/src/main/java/de/visualdependencies/plugin/mysql/MysqlPlugin.java
@@ -0,0 +1,5 @@
+package de.visualdependencies.plugin.mysql;
+
+import de.visualdependencies.plugin.Plugin;
+
+public interface MysqlPlugin extends Plugin {}
---
+++ vdb-mysql-plugin/trunk/src/main/java/de/visualdependencies/plugin/mysql/impl/AbstractMysqlPluginImpl.java
@@ -0,0 +1,14 @@
+package de.visualdependencies.plugin.mysql.impl;
+
+import de.visualdependencies.plugin.Plugin;
+import de.visualdependencies.plugin.impl.AbstractPluginImpl;
+import de.visualdependencies.plugin.mysql.MysqlPlugin;
+
+public class AbstractMysqlPluginImpl extends AbstractPluginImpl implements MysqlPlugin {
+
+	public boolean isCompatible(Plugin otherPlugin) {
+		if (otherPlugin instanceof MysqlPlugin) { return true; }
+		return super.isCompatible(otherPlugin);
+	}
+
+}
