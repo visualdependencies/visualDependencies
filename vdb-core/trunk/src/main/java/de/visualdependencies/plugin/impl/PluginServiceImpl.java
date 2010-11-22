6338a7d33a24e9315b4f0658ebf37ee5c85ebe14
knalli <knallisworld@googlemail.com>
2010-11-22 23:34:17 +0000

    typo in @NonNull usages
    git-svn-id: https://visualdependencies.googlecode.com/svn/vdb@22 56744f29-0aaf-9aa6-4f69-6ecd08e98722

--- vdb-core/trunk/src/main/java/de/visualdependencies/plugin/impl/PluginServiceImpl.java
+++ vdb-core/trunk/src/main/java/de/visualdependencies/plugin/impl/PluginServiceImpl.java
@@ -9,10 +9,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import com.sun.istack.internal.NotNull;
-
 import de.visualdependencies.plugin.Plugin;
 import de.visualdependencies.plugin.PluginService;
+import edu.umd.cs.findbugs.annotations.NonNull;
 
 @Service
 public class PluginServiceImpl implements PluginService {
@@ -21,7 +20,7 @@ public class PluginServiceImpl implements PluginService {
 	private ApplicationContext applicationContext;
 
 	@Override
-	public <T extends Plugin> Collection<T> getPlugins(@NotNull final Class<T> pluginType) {
+	public <T extends Plugin> Collection<T> getPlugins(@NonNull final Class<T> pluginType) {
 
 		Assert.notNull(pluginType, "The plugin type must not be null.");
 
@@ -35,7 +34,7 @@ public class PluginServiceImpl implements PluginService {
 	 * @see ApplicationContext#getBeansOfType(Class)
 	 */
 	@Override
-	public <T extends Plugin> Map<String, T> getPluginsAsMap(@NotNull final Class<T> pluginType) {
+	public <T extends Plugin> Map<String, T> getPluginsAsMap(@NonNull final Class<T> pluginType) {
 		return applicationContext.getBeansOfType(pluginType);
 	}
 
