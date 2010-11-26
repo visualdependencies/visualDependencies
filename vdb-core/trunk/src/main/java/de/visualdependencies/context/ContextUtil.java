be2c81c91981c9a7bc9c301ed05c0872775f0217
knalli <knallisworld@googlemail.com>
2010-11-26 19:08:51 +0000

    vdb (core): improved documentation
    git-svn-id: https://visualdependencies.googlecode.com/svn/vdb@39 56744f29-0aaf-9aa6-4f69-6ecd08e98722

--- vdb-core/trunk/src/main/java/de/visualdependencies/context/ContextUtil.java
+++ vdb-core/trunk/src/main/java/de/visualdependencies/context/ContextUtil.java
@@ -3,12 +3,26 @@ package de.visualdependencies.context;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class ContextUtil {
+/**
+ * The utility class for Spring context management.
+ * 
+ * @author Jan Philipp
+ */
+final public class ContextUtil {
 
+	/**
+	 * Creates and return a new Spring context object.
+	 * 
+	 * This creates a complete new context including data abstraction layer and plugin management. All plugins defined
+	 * in the application's package will recognized.
+	 * 
+	 * @return applicationContext
+	 */
 	public static GenericApplicationContext initialize() {
 
+		// Creating the context for database configuration.
 		final GenericXmlApplicationContext contextDatabase = new GenericXmlApplicationContext(
-		        "classpath:context.xml");
+		        "classpath:context-database.xml");
 		final GenericApplicationContext context = new GenericApplicationContext(contextDatabase);
 		context.refresh();
 
